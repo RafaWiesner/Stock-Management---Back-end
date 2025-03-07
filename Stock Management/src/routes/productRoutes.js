cbac6a76c235dca41a408d90ba9da2c9843e4530
Rafael Wiesner <rafawiesner_msn@hotmail.com>
2025-03-07 08:30:26 -0300

    Alteração nas rotas das requisições

--- Stock Management/src/routes/productRoutes.js
+++ Stock Management/src/routes/productRoutes.js
@@ -4,15 +4,15 @@ import productController from "../controllers/productController.js"
 const router = express.Router();
 
 // Rota para listar todos os produtos (GET)
-router.get('/products', productController.getAllProducts)
+router.get('/', productController.getAllProducts)
 
 //Rota para adicionar um novo produto (POST)
-router.post('/products', productController.createProduct)
+router.post('/', productController.createProduct)
 
 //Rota para atualizar um produto
-router.patch('/products/:id', productController.updateProduct)
+router.patch('/:id', productController.updateProduct)
 
 //Rota para deletar um produto
-router.delete('/products/:id', productController.deleteProduct)
+router.delete('/:id', productController.deleteProduct)
 
 export default router;
