3529d4c26effd5fb6ca8687c11c180ede6f6bf4d
Rafael Wiesner <rafawiesner_msn@hotmail.com>
2025-04-11 11:58:59 -0300

    Separação do database.js para separar responsabilidades

--- Stock Management/src/config/database.js
+++ Stock Management/src/config/database.js
@@ -1,32 +1,12 @@
-import { Sequelize } from "sequelize";
-import { configDotenv } from 'dotenv'; // Importa o método configDotenv
-configDotenv(); // Carrega as variáveis de ambiente do arquivo .env
+import sequelize from "./sequelizeInstance.js";
+import "../models/index.js"; // só importa os models DEPOIS de ter o sequelize pronto
 
-
-
-// Criação da instância do Sequelize usando a URL de conexão do banco de dados
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialect: "postgres",
-    dialectOptions: {
-        ssl: {
-            require: true,
-            rejectUnauthorized: false, // Necessário para conexões seguras
-        },
-    },
-    logging: false,  // Desativa logs SQL (opcional)
-});
-
-import '../models/index.js'
-
-// Teste de conexão com o banco de dados
 sequelize.authenticate()
-    .then(() => {
-        console.log("Conectado ao banco de dados");
-
-        // Sincroniza os modelos com o banco de dados
-        return sequelize.sync({ alter: true }); // Cria/atualiza tabelas sem apagar os dados
-    })
-    .then(() => console.log("Banco de dados sincronizado com sucesso!"))
-    .catch(err => console.error("Erro ao conectar/sincronizar com o banco de dados:", err));
+  .then(() => {
+    console.log("Conectado ao banco de dados");
+    return sequelize.sync({ alter: true });
+  })
+  .then(() => console.log("Banco de dados sincronizado com sucesso!"))
+  .catch(err => console.error("Erro ao conectar/sincronizar com o banco de dados:", err));
 
 export default sequelize;
---
+++ Stock Management/src/config/sequelizeInstace.js
@@ -0,0 +1,17 @@
+// src/config/sequelizeInstance.js
+import { Sequelize } from "sequelize";
+import { configDotenv } from "dotenv";
+configDotenv();
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+  logging: false,
+});
+
+export default sequelize;
