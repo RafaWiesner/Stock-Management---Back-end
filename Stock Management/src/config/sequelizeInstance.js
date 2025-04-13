1f067edb979ddbfd89efdd97100dce0df9c7f046
Rafael Wiesner <rafawiesner_msn@hotmail.com>
2025-04-13 00:52:40 -0300

    ajuste importação index.js no sequelize

--- Stock Management/src/config/sequelizeInstance.js
+++ Stock Management/src/config/sequelizeInstance.js
@@ -3,6 +3,8 @@ import { Sequelize } from "sequelize";
 import { configDotenv } from "dotenv";
 configDotenv();
 
+import "../models/index.js"
+
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
   dialectOptions: {
