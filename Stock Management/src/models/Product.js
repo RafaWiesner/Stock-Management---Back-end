1a792021c8e49f417b37e68f74c4cb3d9dc2cfb2
Rafael Wiesner <rafawiesner_msn@hotmail.com>
2025-04-13 00:09:27 -0300

    arquivo sequelize renomeado

--- Stock Management/src/models/Product.js
+++ Stock Management/src/models/Product.js
@@ -1,5 +1,5 @@
 import { DataTypes } from "sequelize";
-import sequelize from "../config/sequelizeInstace.js";
+import sequelize from "../config/sequelizeInstance.js";
 
 const Product = sequelize.define("Product", {
   id: {
--- Stock Management/src/models/User.js
+++ Stock Management/src/models/User.js
@@ -1,5 +1,5 @@
 import { DataTypes } from "sequelize";
-import sequelize from "../config/sequelizeInstace.js";
+import sequelize from "../config/sequelizeInstance.js";
 
 const User = sequelize.define("User", {
   id: {
